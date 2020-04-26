a2350ae5267ece58b77dfe95f12e09417eecc720
SurendraKMali <37996959+SurendraKMali@users.noreply.github.com>
2020-04-26 22:23:46 +0530

    Curl fix

--- datawarehouse/slack.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -X POST -H 'Content-type: application/json' --data '{"text":"DataWareHouse_Deploy triggered"}' https://hooks.slack.com/services/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
